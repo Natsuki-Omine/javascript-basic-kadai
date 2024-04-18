72be868bffa4e45339ed33578d9d51bf3affa208
Natsuki-Omine <ntk.o8750@gmail.com>
2024-04-18 21:17:31 +0900

    kadai_007フォルダをアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,53 @@
+// 1以上の正の数を変数numに代入
+let num = 25;
+
+// if文で条件分岐
+if (num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 == 0) {
+    console.log('3の倍数です');
+} else if (num % 5 == 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+
+
+// 他の数字をnumに代入して、条件分岐
+num = 60;
+
+if (num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 == 0) {
+    console.log('3の倍数です');
+} else if (num % 5 == 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+
+// ---------------------------------
+num = 17;
+
+if (num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 == 0) {
+    console.log('3の倍数です');
+} else if (num % 5 == 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+
+// ---------------------------------
+num = 24;
+
+if (num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 == 0) {
+    console.log('3の倍数です');
+} else if (num % 5 == 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
